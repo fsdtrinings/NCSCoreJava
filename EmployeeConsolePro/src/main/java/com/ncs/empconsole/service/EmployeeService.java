633d299f9fe43d361914f2a42fc172feeeb0cedc
fsdtrinings <87074240+fsdtrinings@users.noreply.github.com>
2022-07-07 13:00:58 +0530

    Add files via upload

---
+++ EmployeeConsolePro/src/main/java/com/ncs/empconsole/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.ncs.empconsole.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import com.ncs.empconsole.exception.OutofRangeSalaryException;
+import com.ncs.empconsole.model.Employee;
+
+public interface EmployeeService {
+
+	public Employee addEmployee(Employee e)throws OutofRangeSalaryException;
+	public Employee getEmployeeDetails(int searchedEmpId)throws IllegalArgumentException,NoSuchElementException;
+	public Employee getEmployeeDetails(String searchedEmpName);
+	
+	public List<Employee> getAllEmployees();
+	public List<Employee> getAllEmployees(int salaryRange1,int salaryRange2);
+	
+	public Employee updateProject(int searchEmpId,int projectId);
+	
+	
+}
